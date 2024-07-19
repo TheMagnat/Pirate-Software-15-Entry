b97185877673a3dc4f12e6352f90ddb657af342f
obani <obani@pi-et-ro.net>
2024-07-19 16:10:03 +0200

    lobby level WIP

---
+++ Game/Scenes/Levels/lobby.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@onready var player = $Player
+
+func _ready():
+	player.set_camera($CameraInside)
+	$GoOut.body_entered.connect(check_go_out)
+
+func _process(_delta):
+	$CameraOutside.global_position.x = player.global_position.x
+	$CameraOutside.global_position.z = player.global_position.z
+
+func check_go_in(body):
+	if body == player:
+		go(true)
+
+func check_go_out(body):
+	if body == player:
+		go(false)
+
+func go(inside: bool):
+	player.set_physics_process(false)
+	if inside:
+		$GoIn.body_entered.disconnect(check_go_in)
+		$GoOut.body_entered.connect(check_go_out)
+	else:
+		$GoOut.body_entered.disconnect(check_go_out)
+		$GoIn.body_entered.connect(check_go_in)
+	Transition.start(gone.bind(inside))
+	player.set_physics_process(true)
+
+func gone(inside: bool):
+	player.set_camera($CameraInside if inside else $CameraOutside)
--- Game/Scenes/Main/main.gd
+++ Game/Scenes/Main/main.gd
@@ -1,11 +1,13 @@
 extends Node
 
+const lobby := preload("res://Scenes/Levels/lobby.tscn")
+
 func _ready():
 	Save.enable_save = true
 	load_lobby()
 
 func load_lobby():
-	pass
+	$Level.add_child(lobby.instantiate())
 
 func load_level(path: String):
 	pass
