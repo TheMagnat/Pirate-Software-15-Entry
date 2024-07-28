33687abba53bf2ea21c39c64792af283dda42d1e
buloz <lbizeray30@gmail.com>
2024-07-28 03:08:14 +0200

    [level] level 2 props

--- Game/Scenes/Levels/Level_2/2.gd
+++ Game/Scenes/Levels/Level_2/2.gd
@@ -5,4 +5,5 @@ func _ready():
 	Music.play(Music.action1)
 	$Player.changeGroundSound(1)
 	$Player.killed.connect(restart_level)
-	#$finish.body_entered.connect(finish_level)
+	$finish.body_entered.connect(finish_level)
+	$escape.body_entered.connect(escape_level)
