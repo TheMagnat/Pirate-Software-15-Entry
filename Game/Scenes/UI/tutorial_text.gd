69f9a9333c6f8d74d5d1e612d323d51fb57aa361
TheMagnat <guillaume.magniadas@gmail.com>
2024-07-28 13:31:53 +0200

    Better tutorial text

--- Game/Scenes/UI/tutorial_text.gd
+++ Game/Scenes/UI/tutorial_text.gd
@@ -1,4 +1,44 @@
 extends Label3D
 
+# Render distance part
+@export var useRenderDistance: bool = false
+@export var player: Player
+@export var distance: float = 5.0
+
+var isShowing: bool = true
+var fadingTween: Tween
+func startFadeAnimation():
+	if fadingTween:
+		fadingTween.kill()
+	
+	fadingTween = create_tween().set_parallel().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
+	fadingTween.tween_property(self, "modulate:a", int(isShowing), 2.0)
+	fadingTween.tween_property(self, "outline_modulate:a", int(isShowing), 2.0)
+
+func _process(delta):
+	if not useRenderDistance: return
+	
+	var newIsShowing = false
+	if player.global_position.distance_to(global_position) < distance:
+		newIsShowing = true
+	
+	if isShowing != newIsShowing:
+		isShowing = newIsShowing
+		startFadeAnimation()
+
+# Animation part
+var initialScale: Vector3
+var speedScale: float
+var tween: Tween
+func startAnimation():
+	if tween: tween.kill()
+	tween = create_tween().set_loops().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "scale:x", -0.1 * initialScale.x, 5.0 / speedScale).as_relative()
+	tween.tween_property(self, "scale:x", 0.1 * initialScale.x, 5.0 / speedScale).as_relative()
+	tween.tween_property(self, "scale:y", -0.1 * initialScale.y, 5.0 / speedScale).as_relative()
+	tween.tween_property(self, "scale:y", 0.1 * initialScale.y, 5.0 / speedScale).as_relative()
+
 func _ready():
-	$AnimationPlayer.speed_scale = randf_range(0.5, 2.0)
+	initialScale = scale
+	speedScale = randf_range(1.0, 2.0)
+	startAnimation()
