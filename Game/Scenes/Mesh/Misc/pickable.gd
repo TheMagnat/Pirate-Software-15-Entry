fd1fffc000364618a3656a84d20c719b590097b5
TheMagnat <guillaume.magniadas@gmail.com>
2024-07-29 03:37:22 +0200

    Added collectable sound New collectible animation

--- Game/Scenes/Mesh/Misc/pickable.gd
+++ Game/Scenes/Mesh/Misc/pickable.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var itemKey: String
 @export var itemCount := 1
+var collector: Player = null
 
 func _ready():
 	var tween = create_tween().set_loops()
@@ -12,7 +13,25 @@ func _ready():
 func _on_body_entered(body: Node3D):
 	if(body is Player):
 		$Area3D.body_entered.disconnect(_on_body_entered)
+		
 		body.pickup_item.emit(itemKey, itemCount)
-		var t := create_tween()
-		t.tween_property(self, "scale", Vector3(0, 0, 0), 1)
-		t.finished.connect(queue_free)
+		collector = body
+		$AudioStreamPlayer3D.play()
+		
+		global_position = body.global_position + Vector3.UP * 1.5
+		
+		var t := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+
+		t.tween_property(self, "position:y", 1.5, 0.4).as_relative()
+		t.parallel().tween_property(self, "scale", Vector3(0.25, 0.25, 0.25), 0.4).as_relative()
+		
+		t.tween_property(self, "position:y", -1.5, 0.4).as_relative()
+		t.parallel().tween_property(self, "scale", Vector3(0.0, 0.0, 0.0), 0.4)
+		
+		$AudioStreamPlayer3D.finished.connect(queue_free)
+		#t.finished.connect(queue_free)
+
+func _physics_process(delta):
+	if collector:
+		global_position.x = collector.global_position.x
+		global_position.z = collector.global_position.z
