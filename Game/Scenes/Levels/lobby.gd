886d55d1019cd304a171ee236d89478801826fa9
ObaniGemini <robin@pi-et-ro.net>
2024-07-30 13:34:08 +0200

    Add lobby bound check and return to spawn (with fade to black)

--- Game/Scenes/Levels/lobby.gd
+++ Game/Scenes/Levels/lobby.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-@onready var player = $Player
+@onready var player : Player = $Player
+@onready var playerSpawn := player.position
 
 func _ready():
 	Music.play(Music.lobby)
@@ -9,9 +10,16 @@ func _ready():
 	$Radio.finished.connect($Radio.play)
 	$Prototype_Book.enter_craft.connect(enter_craft)
 
+func go_back_to_spawn():
+	set_process(false)
+	Transition.start(
+		func():
+			gone(true)
+			player.position = playerSpawn
+			set_process(true))
+
 var cam_tween : Tween
 func enter_craft(b: bool):
-	
 	if cam_tween: cam_tween.kill()
 	cam_tween = create_tween().bind_node(self).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE).set_parallel(true)
 	cam_tween.tween_property($CameraInside, "position", $Prototype_Book/CamPos.global_position if b else Vector3(5.7, 5.7, 6.1), 0.5)
@@ -28,6 +36,11 @@ var CAMERA_OFFSET := 5.0
 func _process(_delta):
 	$CameraOutside.global_position.x = player.global_position.x
 	$CameraOutside.global_position.z = player.global_position.z + CAMERA_OFFSET
+	
+	### lobby bounds check
+	$CanvasLayer/Line2D.modulate.a = maxf(0.0, player.position.distance_to($CenterPoint.position) * 0.03 - 1.0)
+	if $CanvasLayer/Line2D.modulate.a >= 0.75:
+		go_back_to_spawn() 
 
 func check_go_in(body):
 	if body == player:
@@ -50,4 +63,3 @@ func go(inside: bool):
 
 func gone(inside: bool):
 	player.set_camera($CameraInside if inside else $CameraOutside)
-	
