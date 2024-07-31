a291ea01cb189ca0f673b8ea12bedc647893e925
TheMagnat <guillaume.magniadas@gmail.com>
2024-07-31 07:23:01 +0200

    Applied an "ugly" fix to the boss scaling bug

---
+++ Game/Scenes/Levels/Level_4/boss.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Boss/DeathActionable/Area3D.collision_mask = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scale = Vector3(8.0, 8.0, 8.0)
+	$Boss.scale = Vector3.ONE
